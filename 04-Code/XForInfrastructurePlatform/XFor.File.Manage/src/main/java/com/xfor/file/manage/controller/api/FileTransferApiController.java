641f3e634f3093638b058367703ff8b6dfa07d88
xforliux <xforstudio@163.com>
2020-06-03 15:01:06 +0800

    202006031501

--- 04-Code/XForInfrastructurePlatform/XFor.File.Manage/src/main/java/com/xfor/file/manage/controller/api/FileTransferApiController.java
+++ 04-Code/XForInfrastructurePlatform/XFor.File.Manage/src/main/java/com/xfor/file/manage/controller/api/FileTransferApiController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.FileCopyUtils;
@@ -30,48 +31,25 @@ public class FileTransferApiController {
     private FileTransferService fileTransferService;
 
     @PostMapping("/UploadFile")
-    @ResponseBody
-    public void uploadFile(
+    public ResponseEntity<Boolean> uploadFile(
             @RequestParam String dirPath,
             @RequestParam MultipartFile file)
             throws FileException {
         if (file.isEmpty()) {
             throw new FileException("上传失败，请选择文件");
         }
-        this.fileTransferService.uploadFile(dirPath, file);
+        boolean result = this.fileTransferService.uploadFile(dirPath, file);
+        return new ResponseEntity<Boolean>(result, HttpStatus.OK);
     }
 
     @PostMapping("/DownloadFile")
-    public byte[] downloadFile(@RequestBody String requestBody)
-            throws FileException, JsonProcessingException {
+    public ResponseEntity<InputStreamResource> downloadFile(@RequestBody String requestBody)
+            throws FileException, IOException {
         Map<String, Object> params = new ObjectMapper().readValue(requestBody, Map.class);
         String path = params.get("path").toString();
         //
         File file = this.fileTransferService.downloadFile(path);
-        try (InputStream inputStream = new FileInputStream(file)) {
-            byte[] bytes = inputStream.readAllBytes();
-            return bytes;
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            throw new FileException(ex.getMessage());
-        }
-
-//        try (InputStream inputStream = new FileInputStream(file);
-//             OutputStream outputStream = response.getOutputStream();) {
-//            //设置http头
-//            response.setContentType("application/x-download");
-//            response.addHeader("Content-Disposition", "attachment;filename=" + file.getName());
-//            FileCopyUtils.copy(inputStream, outputStream);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-    }
-
-    @GetMapping("/DownloadFile2")
-    public ResponseEntity<InputStreamResource> downloadFile2(@RequestParam String path)
-            throws FileException, IOException {
         //
-        File file = this.fileTransferService.downloadFile(path);
         FileSystemResource fsr = new FileSystemResource(file.getPath());
         HttpHeaders headers = new HttpHeaders();
         headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
@@ -79,11 +57,15 @@ public class FileTransferApiController {
         headers.add("Pragma", "no-cache");
         headers.add("Expires", "0");
 
-        return ResponseEntity.ok().headers(headers)
+        ResponseEntity<InputStreamResource> result = ResponseEntity
+                .ok()
+                .headers(headers)
                 .contentLength(fsr.contentLength())
                 .contentType(MediaType.parseMediaType("application/octet-stream"))
                 .body(new InputStreamResource(fsr.getInputStream()));
+        return result;
 
+//        File file = this.fileTransferService.downloadFile(path);
 //        try (InputStream inputStream = new FileInputStream(file)) {
 //            byte[] bytes = inputStream.readAllBytes();
 //            return bytes;
@@ -91,6 +73,29 @@ public class FileTransferApiController {
 //            ex.printStackTrace();
 //            throw new FileException(ex.getMessage());
 //        }
+
+    }
+
+    @GetMapping("/DownloadFile2")
+    public ResponseEntity<InputStreamResource> downloadFile2(@RequestParam String path)
+            throws FileException, IOException {
+        //
+        File file = this.fileTransferService.downloadFile(path);
+        //
+        FileSystemResource fsr = new FileSystemResource(file.getPath());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", fsr.getFilename()));
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        ResponseEntity<InputStreamResource> result = ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentLength(fsr.contentLength())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(new InputStreamResource(fsr.getInputStream()));
+        return result;
     }
 
 }
